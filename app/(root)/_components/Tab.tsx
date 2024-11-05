56283cf27772ab0000e863c0b567cc1cccac2b0e
Tanmay Rana <tanmayrana2001@gmail.com>
2024-11-06 00:10:36 +0530

    All code add 4th time commit

--- app/(root)/_components/Tab.tsx
+++ app/(root)/_components/Tab.tsx
@@ -1,29 +1,65 @@
-import Image from "next/image";
+// import Image from "next/image";
+// import React from "react";
+
+// const Tab = ({ label, icon, isActive, onclick }: any) => {
+//   return (
+//     <>
+//       <button
+//         className={`${
+//           isActive ? "text-white border-t-2 border-t-white" : "text-gray-600"
+//         } flex items-center gap-x-1.5 -mt-[1.5px] px-4 py-2 focus:outline-none transition-opacity duration-300 ease-out ${
+//           isActive ? "opacity-100" : "opacity-75"
+//         }`}
+//         onClick={onclick}
+//       >
+//         {icon && (
+//           <Image
+//             src={icon}
+//             alt={`${label} icon`}
+//             className={`w-[11px] h-[11px] ${
+//               isActive ? "brightness-150" : "brightness-50"
+//             }`}
+//           />
+//         )}
+//         {label}
+//       </button>
+//     </>
+//   );
+// };
+
+// export default Tab;
+
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
-const Tab = ({ label, icon, isActive, onclick }: any) => {
+interface TabProps {
+  label: string;
+  icon?: StaticImageData | string;
+  isActive: boolean;
+  onClick: () => void;
+}
+
+const Tab: React.FC<TabProps> = ({ label, icon, isActive, onClick }) => {
   return (
-    <>
-      <button
-        className={`${
-          isActive ? "text-white border-t-2 border-t-white" : "text-gray-600"
-        } flex items-center gap-x-1.5 -mt-[1.5px] px-4 py-2 focus:outline-none transition-opacity duration-300 ease-out ${
-          isActive ? "opacity-100" : "opacity-75"
-        }`}
-        onClick={onclick}
-      >
-        {icon && (
-          <Image
-            src={icon}
-            alt={`${label} icon`}
-            className={`w-[11px] h-[11px] ${
-              isActive ? "brightness-150" : "brightness-50"
-            }`}
-          />
-        )}
-        {label}
-      </button>
-    </>
+    <button
+      className={`${
+        isActive ? "text-white border-t-2 border-t-white" : "text-gray-600"
+      } flex items-center gap-x-1.5 -mt-[1.5px] px-4 py-2 focus:outline-none transition-opacity duration-300 ease-out ${
+        isActive ? "opacity-100" : "opacity-75"
+      }`}
+      onClick={onClick}
+    >
+      {icon && (
+        <Image
+          src={icon}
+          alt={`${label} icon`}
+          className={`w-[11px] h-[11px] ${
+            isActive ? "brightness-150" : "brightness-50"
+          }`}
+        />
+      )}
+      {label}
+    </button>
   );
 };
 
--- app/(root)/_components/TextEllipse.tsx
+++ app/(root)/_components/TextEllipse.tsx
@@ -1,14 +1,37 @@
+// import React from "react";
+
+// const TextEllipse = ({ username, maxLength = 8 }: any) => {
+//   const usernameEllipseStory =
+//     username.length > maxLength
+//       ? `${username.slice(0, maxLength)}...`
+//       : username;
+//   return (
+//     <>
+//       <p className="text-white text-sm mt-1 truncate">{usernameEllipseStory}</p>
+//     </>
+//   );
+// };
+
+// export default TextEllipse;
+
 import React from "react";
 
-const TextEllipse = ({ username, maxLength = 8 }: any) => {
+interface TextEllipseProps {
+  username: string;
+  maxLength?: number;
+}
+
+const TextEllipse: React.FC<TextEllipseProps> = ({
+  username,
+  maxLength = 8,
+}) => {
   const usernameEllipseStory =
     username.length > maxLength
       ? `${username.slice(0, maxLength)}...`
       : username;
+
   return (
-    <>
-      <p className="text-white text-sm mt-1 truncate">{usernameEllipseStory}</p>
-    </>
+    <p className="text-white text-sm mt-1 truncate">{usernameEllipseStory}</p>
   );
 };
 
