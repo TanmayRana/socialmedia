fd5c51be0d9c8e2d09a6ba6fc938bdc1c3bc38a6
Tanmay Rana <tanmayrana2001@gmail.com>
2024-11-06 00:13:15 +0530

    All code add 5th time commit

--- app/(root)/_components/Tab.tsx
+++ app/(root)/_components/Tab.tsx
@@ -36,10 +36,10 @@ interface TabProps {
   label: string;
   icon?: StaticImageData | string;
   isActive: boolean;
-  onClick: () => void;
+  onclick: () => void;
 }
 
-const Tab: React.FC<TabProps> = ({ label, icon, isActive, onClick }) => {
+const Tab: React.FC<TabProps> = ({ label, icon, isActive, onclick }) => {
   return (
     <button
       className={`${
@@ -47,7 +47,7 @@ const Tab: React.FC<TabProps> = ({ label, icon, isActive, onClick }) => {
       } flex items-center gap-x-1.5 -mt-[1.5px] px-4 py-2 focus:outline-none transition-opacity duration-300 ease-out ${
         isActive ? "opacity-100" : "opacity-75"
       }`}
-      onClick={onClick}
+      onClick={onclick}
     >
       {icon && (
         <Image
