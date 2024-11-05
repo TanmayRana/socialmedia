138f161c57f6484c60d3fcfbe917f49b71d7afde
Tanmay Rana <tanmayrana2001@gmail.com>
2024-11-06 00:18:36 +0530

    All code add 6th time commit

--- app/(root)/_components/ReelCard.tsx
+++ app/(root)/_components/ReelCard.tsx
@@ -1,50 +1,50 @@
-import React from "react";
-import reelsData from "./Data/ReelData";
-import Liked from "@/Icons/Like/liked.png";
-import Commented from "@/Icons/Comment/commented.png";
-import Link from "next/link";
-import Image from "next/image";
+// import React from "react";
+// import reelsData from "./Data/ReelData";
+// import Liked from "@/Icons/Like/liked.png";
+// import Commented from "@/Icons/Comment/commented.png";
+// import Link from "next/link";
+// import Image from "next/image";
 
-const ReelCard = () => {
-  return (
-    <>
-      {reelsData.map((reel) => (
-        <Link
-          href="/"
-          key={reel.id}
-          className="lg:w-[24.675%] md:w-[24.675%] sm:w-[32.5%] lg:h-[48vh] md:h-[40vh] sm:h-[35vh] h-[30vh] relative group"
-          style={{
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          <video
-            src={reel.video}
-            title="reel video"
-            loop
-            muted
-            autoPlay
-            className="w-full h-full object-cover"
-          />
-          <div className="hidden group-hover:flex items-center justify-center gap-x-3 absolute top-[50%] left-[50%] translate-x-[-50%]">
-            <div className="flex items-center gap-x-1">
-              <Image src={Liked} alt="like icon" className="w-6 h-6" />
-              <p className="text-base text-white font-medium">
-                {reel.likeCount}
-              </p>
-            </div>
-            <div className="flex items-center gap-x-1">
-              <Image src={Commented} alt="Commented icon" className="w-6 h-6" />
-              <p className="text-base text-white font-medium">
-                {reel.commentCount}
-              </p>
-            </div>
-          </div>
-        </Link>
-      ))}
-    </>
-  );
-};
+// const ReelCard = () => {
+//   return (
+//     <>
+//       {reelsData.map((reel) => (
+//         <Link
+//           href="/"
+//           key={reel.id}
+//           className="lg:w-[24.675%] md:w-[24.675%] sm:w-[32.5%] lg:h-[48vh] md:h-[40vh] sm:h-[35vh] h-[30vh] relative group"
+//           style={{
+//             backgroundPosition: "center",
+//             backgroundSize: "cover",
+//             backgroundRepeat: "no-repeat",
+//           }}
+//         >
+//           <video
+//             src={reel.video}
+//             title="reel video"
+//             loop
+//             muted
+//             autoPlay
+//             className="w-full h-full object-cover"
+//           />
+//           <div className="hidden group-hover:flex items-center justify-center gap-x-3 absolute top-[50%] left-[50%] translate-x-[-50%]">
+//             <div className="flex items-center gap-x-1">
+//               <Image src={Liked} alt="like icon" className="w-6 h-6" />
+//               <p className="text-base text-white font-medium">
+//                 {reel.likeCount}
+//               </p>
+//             </div>
+//             <div className="flex items-center gap-x-1">
+//               <Image src={Commented} alt="Commented icon" className="w-6 h-6" />
+//               <p className="text-base text-white font-medium">
+//                 {reel.commentCount}
+//               </p>
+//             </div>
+//           </div>
+//         </Link>
+//       ))}
+//     </>
+//   );
+// };
 
-export default ReelCard;
+// export default ReelCard;
--- app/(root)/_components/Reels.tsx
+++ app/(root)/_components/Reels.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReelCard from "./ReelCard";
+// import React from "react";
+// import ReelCard from "./ReelCard";
 
-const Reels = () => {
-  return (
-    <>
-      <div className="w-full h-auto flex items-center gap-1 flex-wrap">
-        <ReelCard />
-      </div>
-    </>
-  );
-};
+// const Reels = () => {
+//   return (
+//     <>
+//       <div className="w-full h-auto flex items-center gap-1 flex-wrap">
+//         <ReelCard />
+//       </div>
+//     </>
+//   );
+// };
 
-export default Reels;
+// export default Reels;
